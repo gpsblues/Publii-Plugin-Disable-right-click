b6aafb908ba2acba4c33ac0226b133cdd7597016
gpsblues <vittorio.ramponi@gmail.com>
2024-10-20 19:50:09 +0200

    upload files

---
+++ main.js
@@ -0,0 +1,35 @@
+class DisableRightClick {
+	constructor (API, name, config) {
+		this.API = API;
+		this.name = name;
+		this.config = config;
+	}
+
+	addInsertions () {
+		this.API.addInsertion('publiiHead', this.addStyle, 1, this);
+		this.API.addInsertion('publiiFooter', this.addCode, 1, this);
+	}
+
+	addStyle(rendererInstance) {
+		if(this.config.cssSelection){
+			return `<link rel="stylesheet" href="${rendererInstance.siteConfig.domain}/media/plugins/disableRightClick/disableRC.min.css">`;
+		}
+		return '';
+	}
+
+	addCode() {
+		let myJS = '';
+		if(this.config.jsSelection){
+			myJS += `document.onselectstart=e=>e.preventDefault();`;
+		}
+		if(this.config.jsRightClick){
+			myJS += `document.addEventListener("contextmenu",e=>e.preventDefault())`;
+		}
+		if(myJS){
+			return `<script>${myJS}</script>`;
+		}
+		return '';
+	}
+}
+
+module.exports = DisableRightClick;
